d09f874dfbd974e11df650abdf2d575edbc7679e
Paulo Santana <paulo.santana.r@gmail.com>
2021-05-19 14:33:00 -0300

    test(memccpy): compare with original when n comes before c

--- testft_memccpy.c
+++ testft_memccpy.c
@@ -27,7 +27,40 @@ static int compare_original_memccpy ()
 		if (dest[i] != ft_dest[i])
 			success = 0;
 
-	printf("    Test Original: ");
+	printf("    Test Original (c before n): ");
+	print_success(success);
+	if (!success)
+		explain_expected_diff(dest, ft_dest);
+	return (success);
+}
+
+static int compare_original_memccpy_n()
+{
+	int success = 1;
+	char dest[40];
+	char src[40];
+	char ft_dest[40];
+	char ft_src[40];
+	int	c = '_';
+	int size = 5;
+
+	for (int i = 0; i < 40; i++)
+	{
+		dest[i] = '-';
+		ft_dest[i] = '-';
+		src[i] = 'A';
+		ft_src[i] = 'A';
+	}
+	src[25] = c;
+	ft_src[25] = c;
+	memccpy(&dest[10], &src[10], c, size);
+	ft_memccpy(&ft_dest[10], &ft_src[10], c, size);
+
+	for (int i = 0; i < 40; i++)
+		if (dest[i] != ft_dest[i])
+			success = 0;
+
+	printf("    Test Original (n before c): ");
 	print_success(success);
 	if (!success)
 		explain_expected_diff(dest, ft_dest);
@@ -55,7 +88,8 @@ static int test_return_pointer()
 int	testft_memccpy(void)
 {
 	int	original_test = compare_original_memccpy();
+	int	original_test_n = compare_original_memccpy_n();
 	int	return_test = test_return_pointer();
 
-	return (original_test && return_test);
+	return (original_test && original_test_n && return_test);
 }
