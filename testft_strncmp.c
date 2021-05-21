ea720cfcf8131244ef8a0603c5fce5c02b642455
Paulo Santana <paulo.santana.r@gmail.com>
2021-05-21 14:05:53 -0300

    feat: test big strings

--- testft_strncmp.c
+++ testft_strncmp.c
@@ -91,6 +91,26 @@ static int test_unsigned_char()
 	return (success);
 }
 
+static int test_diff_with_a_big_string()
+{
+	int success = 1;
+	char strA[] = "Did you know? There are only seven billion people in the world.\
+				   If that doesn't blow your mind, I don't know what will.";
+	char strB[] = "Did you know? There are only seven people in the world.\
+				   If that doesn't blow your mind, I don't know what will.";
+	int size = 10;
+
+	int result = strncmp(strA, strB, size);
+	int ft_result = ft_strncmp(strA, strB, size);
+
+	if (result != ft_result)
+		success = 0;
+	print_success("Test with a really big string", success);
+	if (!success)
+		explain_expected_int(result, ft_result);
+	return (success);
+}
+
 int	testft_strncmp()
 {
 	int test1 = test_both_equal();
@@ -98,6 +118,7 @@ int	testft_strncmp()
 	int test3 = test_a_greater_than_b();
 	int test4 = test_difference_after_nul();
 	int test5 = test_unsigned_char();
+	int test6 = test_diff_with_a_big_string();
 
-	return (test1 && test2 && test3 && test4 && test5);
+	return (test1 && test2 && test3 && test4 && test5 && test6);
 }
