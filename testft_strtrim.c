08035def21fa1da95d4c3e42c5d34fdcf06f894f
Paulo Santana <paulo.santana.r@gmail.com>
2021-05-23 21:19:56 -0300

    feat: add more tests for ft_strtrim

--- testft_strtrim.c
+++ testft_strtrim.c
@@ -8,7 +8,6 @@ static int test_trimming_spaces()
 	char set[] = {' ', '\0'};
 	char *expected = "qqqqqqqq";
 
-	(void)expected;
 	char *result = ft_strtrim(test_str, set);
 	if (!result)
 	{
@@ -40,7 +39,6 @@ static int test_trimming_two_characteres()
 	char set[] = {' ', 'a', '\0'};
 	char *expected = "qqqqqqqq";
 
-	(void)expected;
 	char *result = ft_strtrim(test_str, set);
 	if (!result)
 	{
@@ -72,7 +70,6 @@ static int test_trimming_three_characteres()
 	char set[] = {'b', ' ', 'a', '\0'};
 	char *expected = "qqqqqqqq";
 
-	(void)expected;
 	char *result = ft_strtrim(test_str, set);
 	if (!result)
 	{
@@ -104,7 +101,6 @@ static int test_trimming_all_characters()
 	char set[] = {'b', ' ', 'q', 'a', '\0'};
 	char *expected = "";
 
-	(void)expected;
 	char *result = ft_strtrim(test_str, set);
 	if (!result)
 	{
@@ -136,7 +132,6 @@ static int test_with_characteres_that_arent_there()
 	char set[] = {'c', 'g', 'm', 'g', '\0'};
 	char *expected = " a bba b  a qqqqqqqq aab a";
 
-	(void)expected;
 	char *result = ft_strtrim(test_str, set);
 	if (!result)
 	{
@@ -161,6 +156,99 @@ static int test_with_characteres_that_arent_there()
 	return (success);
 }
 
+static int test_with_len_1_but_untrimmable()
+{
+	int success = 1;
+	char *test_str = "a";
+	char set[] = {'c', 'g', 'm', 'g', '\0'};
+	char *expected = "a";
+
+	char *result = ft_strtrim(test_str, set);
+	if (!result)
+	{
+		warn_alloc_fail("ft_strtrim");
+		return (0);
+	}
+	int len = strlen(expected);
+	int ft_len = strlen(result);
+	len = (len < ft_len) ? len : ft_len;
+	for (int i = 0; i <= len; i++)
+		if (expected[i] != result[i])
+			success = 0;
+	char *format = "Test trimming \"cgmb\" from string \"%s\"";
+	char *msg = get_success_msg(format, test_str);
+	if (!msg)
+		return (0);
+	print_success(msg, success);
+	free(msg);
+	if (!success)
+		explain_expected_diff(expected, result, len + 1);
+	free(result);
+	return (success);
+}
+
+static int test_with_len_1_and_trimmable()
+{
+	int success = 1;
+	char *test_str = "a";
+	char set[] = {'c', 'a', 'm', 'g', '\0'};
+	char *expected = "";
+
+	char *result = ft_strtrim(test_str, set);
+	if (!result)
+	{
+		warn_alloc_fail("ft_strtrim");
+		return (0);
+	}
+	int len = strlen(expected);
+	int ft_len = strlen(result);
+	len = (len < ft_len) ? len : ft_len;
+	for (int i = 0; i <= len; i++)
+		if (expected[i] != result[i])
+			success = 0;
+	char *format = "Test trimming \"camb\" from string \"%s\"";
+	char *msg = get_success_msg(format, test_str);
+	if (!msg)
+		return (0);
+	print_success(msg, success);
+	free(msg);
+	if (!success)
+		explain_expected_diff(expected, result, len + 1);
+	free(result);
+	return (success);
+}
+
+static int test_with_empty_set()
+{
+	int success = 1;
+	char *test_str = "abcd kkk opa    ";
+	char set[] = "";
+	char *expected = "abcd kkk opa    ";
+
+	char *result = ft_strtrim(test_str, set);
+	if (!result)
+	{
+		warn_alloc_fail("ft_strtrim");
+		return (0);
+	}
+	int len = strlen(expected);
+	int ft_len = strlen(result);
+	len = (len < ft_len) ? len : ft_len;
+	for (int i = 0; i <= len; i++)
+		if (expected[i] != result[i])
+			success = 0;
+	char *format = "Test trimming \"\" from string \"%s\"";
+	char *msg = get_success_msg(format, test_str);
+	if (!msg)
+		return (0);
+	print_success(msg, success);
+	free(msg);
+	if (!success)
+		explain_expected_diff(expected, result, len + 1);
+	free(result);
+	return (success);
+}
+
 int testft_strtrim()
 {
 	int test1 = test_trimming_spaces();
@@ -168,6 +256,10 @@ int testft_strtrim()
 	int test3 = test_trimming_three_characteres();
 	int test4 = test_trimming_all_characters();
 	int test5 = test_with_characteres_that_arent_there();
+	int test6 = test_with_len_1_but_untrimmable();
+	int test7 = test_with_len_1_and_trimmable();
+	int test8 = test_with_empty_set();
 
-	return (test1 && test2 && test3 && test4 && test5);
+	return (test1 && test2 && test3 && test4 && test5 && test6
+			&& test7 && test8);
 }
